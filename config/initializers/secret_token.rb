c5e36607c5169d097174b4fe3b73dc008bf85001
Anton Kalenik <anton@kalenik.name>
2014-02-05 19:47:53 +0300

    Базовое приложение с установленными настройками PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'e760849928cdb6a27590ed527c918d12c2ffc52307ea135f734f494069b888892a00070ffe3f398b0fafc929665dfa5b527a3fbc28b50d4dfa8e61df0d41ea4a'
