e0185746f687830f98fdae76c5448d29a513a75c
Anton Kalenik <anton@kalenik.name>
2014-02-07 20:24:12 +0300

    сделал rake db:migrate и все изменения из миграции перенеслись в бд

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207161740) do
+ActiveRecord::Schema.define(version: 20140207171954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "statuses", force: true do |t|
-    t.string   "name"
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "statuses", ["user_id"], name: "index_statuses_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "first_name"
